c9db6f58ba31948556154259f27dfd0bae28ed8c
Jinzhi Chen <jinzhi.chen@dolphindb.com>
2025-05-06 15:31:51 +0800

    update

--- server.py
+++ server.py
@@ -16,6 +16,36 @@ DDB_CONFIG = {
 mcp = FastMCP("dolphindb-mcp-server")
 
 @mcp.tool()
+def list_dbs() -> Any:
+    """
+    查看 DolphinDB 中有哪些库
+    """
+    with DatabaseSession(**DDB_CONFIG) as db:
+        return db.execute("getClusterDFSDatabases()")
+    
+@mcp.tool()
+def list_tbs(dbName: str) -> Any:
+    """
+    查看 DolphinDB中，某个分布式库下面有哪些表
+    :param dbName: DolphinDB的库名称，通常是dfs://开头的
+    :return: 分布式库包含的表
+    """
+    with DatabaseSession(**DDB_CONFIG) as db:
+        return db.execute(f"getTables(database(\"{dbName}\"))")
+    
+    
+@mcp.tool()
+def query_table_diskusage(database: str, tableName: str) -> Any:
+    """
+    查看DolphinDB中，某个库表，其磁盘空间占用
+    :param database: DolphinDB的库名称，通常是dfs://开头的
+    :param tableName: DolphinDB的表名称
+    :return 磁盘占用
+    """
+    with DatabaseSession(**DDB_CONFIG) as db:
+        return db.execute(f"use ops; getTableDiskUsage(\"{database}\", \"{tableName}\", byNode=false)")
+
+@mcp.tool()
 def query_dolphindb(script: str) -> Any:
     """
     在 DolphinDB 执行查询
@@ -24,5 +54,11 @@ def query_dolphindb(script: str) -> Any:
     """
     with DatabaseSession(**DDB_CONFIG) as db:
         return db.execute(script)
-    
-print("OKOKOKOKO")
+
+# Run with SSE 
+mcp.run(
+    transport="sse", 
+    host="127.0.0.1",  # Override default host
+    port=8888,         # Override default port
+    log_level="debug"  # Set logging level
+)
